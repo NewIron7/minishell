062264fd65f9f5e16ecb0942d19b06c3fb83dd5d
Damien DELHALT <ddelhalt@e1r11p1.clusters.42paris.fr>
2023-02-14 20:18:56 +0100

    builtins/cd: fix argument count and chdir return check

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: ddelhalt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 22:18:17 by ddelhalt          #+#    #+#             */
-/*   Updated: 2023/02/14 18:25:56 by ddelhalt         ###   ########.fr       */
+/*   Updated: 2023/02/14 19:08:42 by ddelhalt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ int	builtin_cd(char *const argv[])
 {
 	if (*++argv)
 	{
-		if (*argv + 1)
+		if (*(argv + 1))
 			ft_printf_fd(2, "minishell: cd: too many arguments\n");
-		else if (!chdir(*argv))
+		else if (chdir(*argv))
 		{
 			perror("minishell: cd");
 			return (1);
