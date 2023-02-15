50161c05ece0bf1f6a6820550ed4d1de5ce0b4e6
Damien DELHALT <ddelhalt@e1r12p4.clusters.42paris.fr>
2023-02-15 12:23:29 +0100

    minishell: added history to main loop

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ddelhalt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 09:55:11 by ddelhalt          #+#    #+#             */
-/*   Updated: 2023/02/15 11:20:55 by ddelhalt         ###   ########.fr       */
+/*   Updated: 2023/02/15 11:58:31 by ddelhalt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	main_loop(char **envp[])
 		line = readline("minishell$ ");
 		if (!line)
 			builtin_exit(NULL, *envp);
+		add_history(line);
 		ft_printf("%s\n", line);
 		free(line);
 	}
