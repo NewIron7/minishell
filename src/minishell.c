6bc3b85649f13c7b10c4d061b28e3cc6cc565dd1
Damien DELHALT <ddelhalt@e1r5p4.clusters.42paris.fr>
2023-03-14 02:04:12 +0100

    Improved mainloop with better error code handling & other small improvements

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ddelhalt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 09:55:11 by ddelhalt          #+#    #+#             */
-/*   Updated: 2023/03/14 01:24:30 by ddelhalt         ###   ########.fr       */
+/*   Updated: 2023/03/14 02:00:56 by ddelhalt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
@@ -18,15 +18,6 @@ static void	sig_handler(int sig)
 	rl_on_new_line();
 	rl_replace_line("", 1);
 	rl_redisplay();
-}
-
-static void	malloc_err(t_parsing *list_parsing, char *line, char **envp)
-{
-	if (list_parsing)
-		ft_lstclear_parsing(list_parsing);
-	free(line);
-	free_env(envp);
-	exit(EXIT_FAILURE);
 }
 
 static int	get_shell_code(t_list *pipeline)
@@ -47,7 +38,6 @@ void	main_loop(void)
 {
 	char		*line;
 	t_parsing	*list_parsing;
-	char		err_parsing;
 	int			code;
 	char		**envp;
 	struct termios	t_modes;
@@ -61,16 +51,19 @@ void	main_loop(void)
 		line = readline("minishell$ ");
 		if (!line)
 			builtin_exit(NULL, envp);
-		signal(SIGINT, SIG_IGN);
 		add_history(line);
-		err_parsing = parser(line, &list_parsing, envp, code);
-		if (err_parsing == 1)
-			malloc_err(list_parsing, line, envp);
-		if (ft_heredoc(list_parsing) == 0 && err_parsing == 0)
-			eval_exec(subtokens_init(list_parsing, 0, 0, -1), &envp, &pipeline);
-		code = get_shell_code(pipeline);
-		free(line);
-		ft_lstclear_parsing(list_parsing);
-		signal(SIGINT, &sig_handler);
+		if (*line)
+		{
+			signal(SIGINT, SIG_IGN);
+			code = parser(line, &list_parsing, envp, code);
+			if (!code && ft_heredoc(list_parsing) == 0)
+			{
+				eval_exec(subtokens_init(list_parsing, 0, 0, -1), &envp, &pipeline);
+				code = get_shell_code(pipeline);
+			}
+			free(line);
+			ft_lstclear_parsing(list_parsing);
+			signal(SIGINT, &sig_handler);
+		}
 	}
 }
