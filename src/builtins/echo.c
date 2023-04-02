2ccb0c87926dcbef8f5cdae4e423a7d2034b808f
hboissel <hboissel@student.42.fr>
2023-04-02 11:10:00 +0200

    echo.c normed

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,11 +6,24 @@
 /*   By: ddelhalt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 22:19:32 by ddelhalt          #+#    #+#             */
-/*   Updated: 2023/03/31 15:57:24 by ddelhalt         ###   ########.fr       */
+/*   Updated: 2023/04/02 11:09:10 by hboissel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static char	check_newline(int newline, int fd)
+{
+	if (newline)
+	{
+		if (write(fd, "\n", 1) < 0)
+		{
+			perror("echo: write error");
+			return (1);
+		}
+	}
+	return (0);
+}
 
 int	builtin_echo(char *const argv[], int fd)
 {
@@ -27,27 +40,15 @@ int	builtin_echo(char *const argv[], int fd)
 		while (*argv)
 		{
 			if (write(fd, *argv, ft_strlen(*argv)) < 0)
-			{
-				perror("echo: write error");
-				return (1);
-			}
+				return (perror("echo: write error"), 1);
 			if (*++argv)
 			{
 				if (write(fd, " ", 1) < 0)
-				{
-					perror("echo: write error");
-					return (1);
-				}
+					return (perror("echo: write error"), 1);
 			}
 		}
 	}
-	if (newline)
-	{
-		if (write(fd, "\n", 1) < 0)
-		{
-			perror("echo: write error");
-			return (1);
-		}
-	}
+	if (check_newline(newline, fd))
+		return (1);
 	return (0);
 }
