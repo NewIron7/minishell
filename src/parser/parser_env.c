3af71f6654d405c9eeb095089da343e78d1bb2e6
hboissel <hboissel@student.42.fr>
2023-04-03 17:23:28 +0200

    replace_content done

--- src/parser/parser_env.c
+++ src/parser/parser_env.c
@@ -6,7 +6,7 @@
 /*   By: hboissel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 18:50:56 by hboissel          #+#    #+#             */
-/*   Updated: 2023/04/03 15:43:34 by hboissel         ###   ########.fr       */
+/*   Updated: 2023/04/03 17:21:54 by hboissel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "parser.h"
@@ -24,12 +24,12 @@ char	expand_elems(t_parsing *elem, t_parsing **next)
 static char	expand_var(t_expand split[], t_env envp)
 {
 	int		i;
-	char	**content_env[];
+	char	**content_env[2];
 	char	err;
 
 	content_env[0] = envp.env;
 	i = 0;
-	while (split[i])
+	while (split[i].str)
 	{
 		if (split[i].type == DFL || split[i].type == DBL)
 		{
@@ -38,6 +38,88 @@ static char	expand_var(t_expand split[], t_env envp)
 			if (err)
 				return (err);
 		}
+		i++;
+	}
+	return (0);
+}
+
+static char	init_replace_content(t_expand split[], t_parsing *elem)
+{
+	char	*ncontent;
+
+	if (split && split->str)
+	{
+		ncontent = ft_strdup(split->str);
+		if (ncontent == NULL)
+			return (1);
+		free(elem->content);
+		elem->content = ncontent;
+		return (0);
+	}
+	return (1);
+}
+
+static char	join_on_elem(char **content, char *str)
+{
+	char	*ncontent;
+
+	ncontent = ft_strjoin(*content, str);
+	if (ncontent == NULL)
+		return (1);
+	free(*content);
+	*content = ncontent;
+	return (0);
+}
+
+static char	replace_content_elem(t_expand split[], t_parsing *elem)
+{
+	int	i;
+
+	if (init_replace_content(split, elem))
+		return (1);
+	i = 1;
+	while (split[i].str)
+	{
+		if (join_on_elem(&elem->content, split[i].str))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static char	create_elem_replace_content(t_expand split[], t_parsing **elem)
+{
+	t_parsing	*nelem;
+	char		*str;
+	t_parsing	*tmp;
+
+	str = ft_strdup("");
+	if (str == NULL)
+		return (1);
+	nelem = ft_lstnew_parsing(str, ARG);
+	if (nelem == NULL)
+		return (1);
+	if (replace_content_elem(split, nelem))
+		return (free(nelem), 1);
+	tmp = (*elem)->next;
+	(*elem)->next = nelem;
+	nelem->prev = (*elem);
+	nelem->next = tmp;
+	*elem = tmp;
+	return (0);
+}
+
+static char	replace_content(t_expand *split[], t_parsing *elem)
+{
+	int			i;
+
+	if (replace_content_elem(split[1], elem))
+		return (1);
+	i = 1;
+	while (split[i])
+	{
+		if (create_elem_replace_content(split[i], &elem))
+			return (1);
 		i++;
 	}
 	return (0);
